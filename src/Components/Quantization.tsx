245f52125e236496c0c90d5b8673669edc7a8338
Perry Xie <perry.xie1@gmail.com>
2025-05-19 20:25:50 -0700

    todo

--- src/Components/Quantization.tsx
+++ src/Components/Quantization.tsx
@@ -1,3 +1,13 @@
+
+// Point Filtering <- probably what we want
+//   - Trilinear Filtering
+//   - Anisotrophic Filtering
+// Bilinear filtering <- blurring
+
+// Dithering -> reduce color pallete
+//   - Ordered Dithering -> https://en.wikipedia.org/wiki/Ordered_dithering
+
+
 const Quantization = () => {
   return (
     <div>
