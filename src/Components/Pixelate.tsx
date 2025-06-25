53d52560007c1196654cafaefdac8ab6c145c63e
thepshay <perry.xie1@gmail.com>
2025-06-24 20:50:58 -0700

    Start on dither options

--- src/Components/Pixelate.tsx
+++ src/Components/Pixelate.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, type ChangeEvent } from "react";
+import { useEffect, useRef, useState, type ChangeEvent, type SelectHTMLAttributes } from "react";
 import { medianCut } from "../utils/quantizationUtils";
 import { copyCanvas, fillCanvas, getPixelMatrix } from "../utils/utils";
 import { getDownscaleMatrix, getUpscalePixelMatrix, orderDither } from "../utils/pixelateUtils";
@@ -16,11 +16,17 @@ const Pixelate = ({
   imageAdded,
   pixelsData,
 }: PixelateProps) => {
+  const DOWN_DITHER_UP = 0;
+  const DITHER_DOWN_UP = 1;
+  const DOWN_UP = 2;
+  const DOWN_DIST_UP = 3;
+  const DIST_DOWN_UP = 4;
 
   const pixelateCanvasRef = useRef(null);
-  const [power, setPower] = useState(1);
+  const [power, setPower] = useState(3);
   const nonTransparentPixels = pixelsData.filter((color) => color.a !== 0);
-  const [pixelSize, setPixelSize] = useState(4);
+  const [pixelSize, setPixelSize] = useState(8);
+  const [pixelateType, setPixeateType] = useState(DOWN_DITHER_UP);
 
   useEffect(() => {
     if (!canvasRef.current) {
@@ -36,13 +42,16 @@ const Pixelate = ({
     copyCanvas(canvas, pixelateCanvasRef);
   }, [imageAdded]);
 
-
   const handleUpdatePower = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newPower = Number(e.target.value);
     setPower(newPower);
   }
 
-  const handlePixelate = () => {
+  const handleSelectPixelateOption = (e: ChangeEvent<HTMLSelectElement>) => {
+    setPixeateType(Number(e.target.value));
+  }
+
+  const handlePixelateDownDitherUp = () => {
     if (!canvasRef.current) {
       console.log('no original canvas ref');
       return;
@@ -56,15 +65,50 @@ const Pixelate = ({
     const originalCanvas = canvasRef.current as HTMLCanvasElement;
     const colorPalette = medianCut(nonTransparentPixels, power);
     const pixelMatrix = getPixelMatrix(pixelsData, originalCanvas.width, originalCanvas.height);
-    const downscalePixelMatrix = getDownscaleMatrix(pixelMatrix, pixelSize);
-    const ditheredMatrix = orderDither(downscalePixelMatrix, 4, colorPalette);
-    const upscalePixelMatrix = getUpscalePixelMatrix(ditheredMatrix, pixelSize);
+
+    // 3. downscale -> euclidean mapping -> upscale
+
     const mapCanvas = pixelateCanvasRef.current as HTMLCanvasElement
     const ctx = mapCanvas.getContext('2d') as CanvasRenderingContext2D;
 
-    fillCanvas(ctx, upscalePixelMatrix, mapCanvas.width, mapCanvas.height);
+    const processedMatrix = handlePixelation(pixelMatrix, colorPalette);
+
+    fillCanvas(ctx, processedMatrix, mapCanvas.width, mapCanvas.height);
 
     console.log('finish pixelate');
+  }
+
+  const handlePixelation = (
+    pixelMatrix: {
+      r: number,
+      g: number,
+      b: number,
+    }[][],
+    colorPalette: {
+      r: number,
+      g: number,
+      b: number,
+    }[]) => {
+    let returnMatrix;
+
+    if (pixelateType === DOWN_DITHER_UP) {
+      // 1. downscale -> dither -> upscale
+      const downscalePixelMatrix = getDownscaleMatrix(pixelMatrix, pixelSize);
+      const ditheredMatrix = orderDither(downscalePixelMatrix, 4, colorPalette);
+      returnMatrix = getUpscalePixelMatrix(ditheredMatrix, pixelSize);
+    } else if (pixelateType === DITHER_DOWN_UP) {
+      // 2. dither -> downscale -> upscale
+      const ditheredMatrix = orderDither(pixelMatrix, 4, colorPalette);
+      const downscaledMatrix = getDownscaleMatrix(ditheredMatrix, pixelSize);
+      returnMatrix = getUpscalePixelMatrix(downscaledMatrix, pixelSize);
+    } else if (pixelateType === DOWN_UP) {
+      const downscaledMatrix = getDownscaleMatrix(pixelMatrix, pixelSize);
+      returnMatrix = getUpscalePixelMatrix(downscaledMatrix, pixelSize);
+    } else {
+      returnMatrix = pixelMatrix;
+    }
+
+    return returnMatrix;
   }
 
   const updatePixelSize = (e: ChangeEvent<HTMLInputElement>) => {
@@ -102,7 +146,7 @@ const Pixelate = ({
           <div>Pixel Size</div>
           <div className="radio-container">
             {new Array(9).fill(null).map((_, idx) => {
-              const value = idx * idx;
+              const value = Math.pow(2, idx);
               return (
                 <div key={idx} className="radio-item">
                   <input
@@ -118,9 +162,19 @@ const Pixelate = ({
             })}
           </div>
         </div>
+        <div>
+          <div>Pixelate options</div>
+          <select onChange={handleSelectPixelateOption}>
+            <option value={DOWN_DITHER_UP}>Downscale | Dither | Upscale</option>
+            <option value={DITHER_DOWN_UP}>Dither | Downscale | Upscale</option>
+            <option value={DOWN_UP}>Downscale | Upscale</option>
+            <option value={DOWN_DIST_UP}>Downscale | Distance | Upscale</option>
+            <option value={DIST_DOWN_UP}>Distance | Downscale | Upscale</option>
+          </select>
+        </div>
       </div>
       <br />
-      <button onClick={handlePixelate}>Pixelate</button>
+      <button onClick={handlePixelateDownDitherUp}>Pixelate Down Dither Up</button>
       <br />
       <canvas
         id="pixelate-canvas"
